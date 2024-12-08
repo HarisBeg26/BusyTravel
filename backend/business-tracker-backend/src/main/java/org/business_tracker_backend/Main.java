04c4cf91939cbb9be26d833e477ad1cc49a80ad3
HarisBeg26 <univerzing.begovic25@gmail.com>
2024-12-08 18:12:32 +0100

    Renamed the project

--- backend/business-tracker-backend/src/main/java/org/business_tracker_backend/Main.java
+++ backend/business-tracker-backend/src/main/java/org/business_tracker_backend/Main.java
@@ -1,4 +1,4 @@
-package org.business-tracker-backend;
+package org.business_tracker_backend;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
