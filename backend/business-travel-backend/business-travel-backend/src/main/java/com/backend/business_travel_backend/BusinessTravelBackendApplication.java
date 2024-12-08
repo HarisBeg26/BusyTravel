dc89f17e70efc0678d7ded879d3cf6ee532cc87c
HarisBeg26 <univerzing.begovic25@gmail.com>
2024-12-08 17:59:31 +0100

    Refactored the backend project from Flask to Java Springboot

---
+++ backend/business-travel-backend/business-travel-backend/src/main/java/com/backend/business_travel_backend/BusinessTravelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.business_travel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessTravelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessTravelBackendApplication.class, args);
+	}
+
+}
---
+++ backend/business-travel-backend/business-travel-backend/src/test/java/com/backend/business_travel_backend/BusinessTravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.business_travel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessTravelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
