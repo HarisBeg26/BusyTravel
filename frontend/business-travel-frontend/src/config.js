9a45b4d272e4282c12f55b02d0f5c2f4b73dbcb7
HarisBeg26 <univerzing.begovic25@gmail.com>
2025-10-05 23:42:55 +0200

    Deployed backend on Render

--- frontend/business-travel-frontend/src/config.js
+++ frontend/business-travel-frontend/src/config.js
@@ -4,7 +4,7 @@
 const config = {
   // API base URL that changes based on environment
   apiBaseUrl: import.meta.env.PROD
-    ? 'https://business-travel-backend.onrender.com/api' // Replace with your actual Render backend URL
+    ? 'https://busytravel.onrender.com/' // Replace with your actual Render backend URL
     : 'http://localhost:3000/api'
 };
 
